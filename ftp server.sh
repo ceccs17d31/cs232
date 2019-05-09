fdd01356653065e58e3313f44da1f0dddfa3c225
ceccs17d31 <43372142+ceccs17d31@users.noreply.github.com>
2019-05-09 10:03:15 +0530

    Add files via upload

---
+++ ftp server.sh
@@ -0,0 +1,21 @@
+
+
+##FTP Server
+
+sudo apt-get install vsftpd
+sudo nano /etc/vsftpd.conf #Change anon_users to YES.
+sudo vsftpd #Starts server.
+
+##Go to Web Browser.
+
+ftp://localhost
+ 
+##OR IP-address.
+
+##To open localhost from remote server.
+
+ssh cs17d31@192.168.0.30
+ftp -p 192.168.0.189  ##Opens Ip address of localhost in passive mode
+## Name: s4d2 & pass
+
+ftp>ls
